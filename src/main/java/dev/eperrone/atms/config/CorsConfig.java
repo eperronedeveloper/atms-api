d75e79cab7e76a16047806e7061f828740be0b9d
eperronedesarrollo <eperrone.developer@gmail.com>
2026-03-10 01:05:14 -0300

    Create CorsConfig.java

---
+++ src/main/java/dev/eperrone/atms/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package dev.eperrone.atms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Nonnull;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@Nonnull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://eperrone.dev",
+                                "https://www.eperrone.dev",
+                                "http://localhost:5500",
+                                "http://127.0.0.1:5500"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
